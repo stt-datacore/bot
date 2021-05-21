115ca303738bcf4f503aaacf353ad98ccea7780e
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-05-22 02:56:07 +1000

    Use canonical emoji references, rather than emumerating over the local guild.
    Fixes #36

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -2,13 +2,34 @@ import { Message } from 'discord.js';
 import NodeCache from 'node-cache';
 
 export function getEmoteOrString(message: Message, emojiName: string, defaultString: string): string {
-	if (message.guild) {
-		let emoji = message.guild.emojis.cache.find(emoji => emoji.name === emojiName);
-		if (emoji) {
-			return emoji.toString();
-		}
+	switch (emojiName) {
+		case 'credits':
+			return '<:credits:835521627944124416>';
+		case 'chrons':
+			return '<:chrons:730399914005102623>';	
+		case 'dil':
+			return '<:dil:730399914332389446>';
+		case 'honor':
+			return '<:honor:730399914592567368>';
+		case 'shuttle':
+			return '<:shuttle:835521637612257321>';
+		case 'cmd':
+			return '<:cmd:730399913413967963>';
+		case 'dip':
+			return '<:dip:730399914269474946>';
+		case 'eng':
+			return '<:eng:730399913707438181>';
+		case 'med':
+			return '<:med:730399913799581777>'; 
+		case 'sci':
+			return '<:sci:730399914449961030>';
+		case 'sec':
+			return '<:sec:730399914349297785>';
+		default:
+			// We call this function for Ship abilities (attack,evasion,accuracy,shield regen) 
+			// but we don't have any emoji for them, so they always fallback to text.
+			return defaultString;
 	}
-	return defaultString;
 }
 
 export let MessageCache = new NodeCache({ stdTTL: 600 });
