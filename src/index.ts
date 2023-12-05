b63f26310cdb4488559d6dcad24d71d383b3ee49
Nathaniel Moschkin <precise.tester@gmail.com>
2023-12-05 16:33:52 -0500

    remove sync with alter

--- src/index.ts
+++ src/index.ts
@@ -43,8 +43,7 @@ client.login(process.env.BOT_TOKEN);
 const config = JSON.parse(fs.readFileSync(process.env.CONFIG_PATH!, 'utf8'));
 const devGuilds = Object.keys(config.guilds).filter((id) => config.guilds[id].dev === true);
 
-sequelize.sync().then(async () => {
-	await Profile.sync({ alter: true });
+sequelize.sync().then(() => {
 	Logger.info('Database connection established');
 });
 
