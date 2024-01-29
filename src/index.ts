9150674cb1b44e08661221f7bec1e2038b8472c3
Nathaniel Moschkin <precise.tester@gmail.com>
2024-01-29 18:47:43 -0500

    code cleanup.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import { sequelize } from './sequelize';
 import { runImageAnalysis } from './commands/imageanalysis';
 import { Commands } from './commands';
 import levenshtein from 'js-levenshtein';
-import { shipSum } from './utils/ships';
 
 const Yargs = require('yargs/yargs');
 
